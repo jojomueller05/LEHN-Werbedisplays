0f36e500a04db377e6e5e1ce894269c3b421c82c
jojomueller05 <j_j_m@bluewin.ch>
2023-03-31 09:28:26 +0200

    corrected sql script

--- build/sql-install/init.sql
+++ build/sql-install/init.sql
@@ -1,29 +1,30 @@
 -- Build SQL DB
+
 CREATE TABLE tbl_user(
-	id_user int PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-	email text unique,
-	password text,
-	logged_in boolean,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	last_login_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id_user int PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    email text unique,
+    password text,
+    logged_in boolean,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tbl_shop(
-	id_shop int PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-	name text,
-	screens int
+    id_shop int PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    name text,
+    screens int
 );
 
 CREATE TABLE tbl_image (
-	id_image int PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-	file_path text
+    id_image int PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL,
+    file_path text
 );
 
 CREATE TABLE tbl_screen (
-	id_screen int PRIMARY KEY,
-	fs_shop int,
-	fs_image int,
-	screen int,
-	FOREIGN KEY (fs_shop) REFERENCES tbl_shop(id_shop),
-	FOREIGN KEY (fs_image) REFERENCES tbl_image(id_image)
+    id_screen int PRIMARY KEY,
+    fs_shop int,
+    fs_image int,
+    screen int,
+    FOREIGN KEY (fs_shop) REFERENCES tbl_shop(id_shop),
+    FOREIGN KEY (fs_image) REFERENCES tbl_image(id_image)
 );
