250da8fed12e7535095038b6936a2500d4b800ab
jojomueller05 <j_j_m@bluewin.ch>
2023-04-05 11:43:14 +0200

    footer gflickt

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,30 +1,30 @@
-<footer class="fixed-bottom p-3 mt-5 pt-5 bg-dark-subtle text-emphasis-dark">
+<footer class="p-3 mt-5 pt-5 bg-dark-subtle text-emphasis-dark fixed-bottom">
     <div class="row">
       <div class="col-12 col-md-4 text-center">
         <h4 class="fs-5">Contact</h4>
         <div class="row">
-            <div class="col">
-              <ul style="list-style-type: none;">
-                <li>
-                  <img src="app\img\ticket.png" alt="">
-                  <a class="text-decoration-none text-dark" href="https://www.servicedesk.lehner-versand.ch">servicedesk.lehner-versand.ch</a>
-                </li>
-                <li>
-                  <img src="app\img\email.png" alt="">
-                  <a class="text-decoration-none text-dark" href="mailto:servicedesk@lehner-versand.ch">servicedesk@lehner-versand.ch</a>
-                </li>
-                <li>
-                  <img src="app\img\phone-call.png" alt="">
-                  <a class="text-decoration-none text-dark" href="tel:00419267400">041 926 74 00</a>
-                </li>
-              </ul>
-            </div>
+          <div class="col">
+            <ul style="list-style-type: none;">
+              <li>
+                <img src="app\img\ticket.png" alt="">
+                <a class="text-decoration-none text-dark" href="https://www.servicedesk.lehner-versand.ch">servicedesk.lehner-versand.ch</a>
+              </li>
+              <li>
+                <img src="app\img\email.png" alt="">
+                <a class="text-decoration-none text-dark" href="mailto:servicedesk@lehner-versand.ch">servicedesk@lehner-versand.ch</a>
+              </li>
+              <li>
+                <img src="app\img\phone-call.png" alt="">
+                <a class="text-decoration-none text-dark" href="tel:00419267400">041 926 74 00</a>
+              </li>
+            </ul>
           </div>
+        </div>
       </div>
       <div class="col-12 col-md-4 text-center">
         <h4 class="fs-5">About</h4>
         <p>Checkout the Project on <a href="https://github.com/jojomueller05/LEHN-Werbedisplays">GitHUB</a>
-        <br> ~ Joël Julien Müller
+          <br> ~ Joël Julien Müller
         </p>
       </div>
       <div class="col-12 col-md-4 text-center">
@@ -34,5 +34,6 @@
       </div>
     </div>
   </footer>
+  
 </body>
 </html>
