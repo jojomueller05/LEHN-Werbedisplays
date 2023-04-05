78c70e5ef8eebd97b3e44c3f9010fb41c7ea257e
jojomueller05 <j_j_m@bluewin.ch>
2023-04-05 11:20:10 +0200

    created header and footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,2 +1,38 @@
+<footer class="fixed-bottom p-3 mt-5 pt-5 bg-dark-subtle text-emphasis-dark">
+    <div class="row">
+      <div class="col-12 col-md-4 text-center">
+        <h4 class="fs-5">Contact</h4>
+        <div class="row">
+            <div class="col">
+              <ul style="list-style-type: none;">
+                <li>
+                  <img src="app\img\ticket.png" alt="">
+                  <a class="text-decoration-none text-dark" href="https://www.servicedesk.lehner-versand.ch">servicedesk.lehner-versand.ch</a>
+                </li>
+                <li>
+                  <img src="app\img\email.png" alt="">
+                  <a class="text-decoration-none text-dark" href="mailto:servicedesk@lehner-versand.ch">servicedesk@lehner-versand.ch</a>
+                </li>
+                <li>
+                  <img src="app\img\phone-call.png" alt="">
+                  <a class="text-decoration-none text-dark" href="tel:00419267400">041 926 74 00</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+      </div>
+      <div class="col-12 col-md-4 text-center">
+        <h4 class="fs-5">About</h4>
+        <p>Checkout the Project on <a href="https://github.com/jojomueller05/LEHN-Werbedisplays">GitHUB</a>
+        <br> ~ Joël Julien Müller
+        </p>
+      </div>
+      <div class="col-12 col-md-4 text-center">
+        <a href="https://www.lehner-versand.ch">
+          <img src="app\img\lehner-logo.png" alt="Lehner Versand Logo">
+        </a>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
--- app/views/header.php
+++ app/views/header.php
@@ -10,23 +10,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="./app/img/lehner-logo.png" alt="">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Settings</a>
-            </ul>
-          </div>
+  <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand mx-3" href="#">
+        <img src="app\img\lehner-logo.png" alt="Lehner Versand Logo" class="img-fluid">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto text-light">
+          <a class="nav-link active fs-3 mx-5" aria-current="page" href="#">Dashboard</a>
+          <a class="nav-link fs-3 mx-5" href="#">Settings</a>
         </div>
-      </nav>   
-
+      </div>
+    </div>
+  </nav>
+  
