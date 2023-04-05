808dd14abbf5146db945447f568cf119a8ea8ae9
jojomueller05 <j_j_m@bluewin.ch>
2023-04-05 14:45:33 +0200

    fixed some stuff

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,6 +1,6 @@
 <footer class="p-3 mt-auto pt-5 text-white  bg-dark">
     <div class="row">
-      <div class="col-12 col-md-4 text-center">
+      <div class="col-12 col-md-4 text-center mt-5">
         <h4 class="text-uppercase mb-4">Contact</h4>
         <div class="container w-50 text-start">
             <div class="col">
@@ -17,22 +17,24 @@
             </div>
         </div>
       </div>
-      <div class="col-12 col-md-4 text-center">
-        <h4 class="text-uppercase mb-4">About</h4>
+      <div class="col-12 col-md-4 text-center mt-5">
+        <h4 class="text-uppercase mb-4 ">About</h4>
         <p>Checkout the Project on <a href="https://github.com/jojomueller05/LEHN-Werbedisplays">GitHUB</a>
           <br> ~ Joël Julien Müller
         </p>
       </div>
-      <div class="col-12 col-md-4 text-center">
+      <div class="col-12 col-md-4 d-flex justify-content-center align-items-center mt-5">
         <a href="https://www.lehner-versand.ch">
           <img src="app\img\lehner-logo.png" alt="Lehner Versand Logo">
         </a>
       </div>
+      
     </div>
     <div class="text-center p-3 border-top border-white mt-5">
         © 2023 Copyright: No Copyright
       </div>
   </footer>
+  
   <style>
     body {
   min-height: 100vh;
--- index.php
+++ index.php
@@ -5,36 +5,40 @@ require_once('./app/views/header.php')
 <!--Content goes here-->
 <div class="container min-vh-100">
     <div class="row justify-content-center mt-5">
-      <div class="col-sm-6 col-md-4">
-        <div class="card">
+      <div class="col-sm-8 col-md-8 col-lg-6">
+        <div class="card p-4">
           <div class="card-body">
-            <h5 class="card-title text-center mb-4">Login</h5>
+            <h2 class="card-title text-center mb-4">Login</h2>
+            <p class="text-black-50 text-center mb-5">Please enter your login and password!</p>
             <form action="" method="post">
               <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email" required>
+                <label for="email" class="form-label fs-5">Email address</label>
+                <input type="email" class="form-control fs-5" id="email" placeholder="Enter email" required>
               </div>
               <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
+                <label for="password" class="form-label fs-5">Password</label>
                 <div class="input-group">
-                  <input type="password" class="form-control" id="password" placeholder="Enter password" required>
+                  <input type="password" class="form-control fs-5" id="password" placeholder="Enter password" required>
                   <button class="btn btn-outline-secondary" type="button" id="show-password">
                     <i class="bi bi-eye"></i>
                   </button>
                 </div>
               </div>
               <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" class="btn btn-primary fs-5">Login</button>
               </div>
             </form>
-            <div class="text-center mt-3">
-              <a href="#">Create Account</a>
+            <div class="mt-5">
+                <p class="mb-0 text-center">Don't have an account? <a href="#!" class="fw-bold">Sign Up</a>
+                </p>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  
+  
   
   <script>
     const showPasswordButton = document.getElementById("show-password");
