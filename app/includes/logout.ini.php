63450b6c5d31ab3f8780eaab514db8d44f0217fa
jojomueller05 <j_j_m@bluewin.ch>
2023-04-06 14:35:14 +0200

    added logout function

--- app/includes/logout.ini.php
+++ app/includes/logout.ini.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
+require_once ('dbh.inc.php');
+
+$stmt = $pdo->prepare('UPDATE user SET logged_in = false WHERE email = ?');
+$stmt->execute([$_SESSION['user_mail']]);
+
 session_unset();
 session_destroy();
 header("Location: ../../index.php");
 exit;
+
 ?>
